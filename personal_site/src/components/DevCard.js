f6131a813de52bc0dfd55420b7a83761dfaa182a
Kamau-LCC <160455079+Kamau-G@users.noreply.github.com>
2024-08-12 21:15:00 -0700

    Home page updates
    Laid the foundation for the landing page.
    Very pleased with the responsiveness of the design layout.

---
+++ personal_site/src/components/DevCard.js
@@ -0,0 +1,10 @@
+import {useState} from 'react';
+
+function DevCard(){
+    return(
+        <div className='DevCard col-6'>
+            DevCard
+        </div>
+    );
+}
+export default DevCard;
---
+++ personal_site/src/views/vHome.js
@@ -0,0 +1,10 @@
+import DevCard from "../components/DevCard";
+
+function vHome(){
+    return(
+    <div>
+        <DevCard/><DevCard/>
+    </div>
+    );
+}
+export default vHome;
