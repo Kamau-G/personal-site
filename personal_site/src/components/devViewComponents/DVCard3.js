df0e4480c92f61bc0fc97421cd1f5fb9425206c6
Kamau <160455079+Kamau-G@users.noreply.github.com>
2024-08-23 10:16:13 -0700

    Update DVCard3.js

--- personal_site/src/components/devViewComponents/DVCard3.js
+++ personal_site/src/components/devViewComponents/DVCard3.js
@@ -7,7 +7,17 @@ function DevViewCard3({setNav}){
     }
     return(
         <div className='DevViewCard3 slide-in-left' onClick={function(){}}>
-            Card 3
+            <text>
+            <h1>About the Developer:</h1>
+            Greetings,<br/>
+            My name is Kamau G. or Mr. G to my commrads<br/>
+            I am a software engineer with ~1 yr of professional softare engineering experience and 2 yrs of working as a webpack developer for a small local company.<br/><br/>
+            How I assist companies:<br/>
+            I pride myself on keeping a good head at the workplace, I attempt to make every interaction enjoyable, and effective.
+            I pride my self on finding solutions that are tailored to the needs of the overall projects with an emphasis on compartmentalization and decentralization of components.<br/>
+            <br/>
+            Think your company would be a good match? Take the quiz to find out if your company is something I am looking for:
+            </text>
         </div>
     );
 }
